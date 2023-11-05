70a258e49722e277b000de1c8b934b1f133ea2b6
ordamari <ordamari971@gmail.com>
2023-11-05 23:58:26 +0200

    Add logo and title

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser'
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
-    constructor() {}
+export class HeaderComponent implements OnInit {
+    constructor(private sanitizer: DomSanitizer) {}
+    imageSrc: SafeResourceUrl | undefined
+
+    ngOnInit() {
+        const imagePath = 'assets/images/logo.png' // Replace with the path to your image
+        this.imageSrc = this.sanitizer.bypassSecurityTrustResourceUrl(imagePath)
+    }
 }
